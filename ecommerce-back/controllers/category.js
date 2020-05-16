ef9eb71e76a9dd0e86734528cd6b72d6653ab6d1
RJDdev <rahnidemeis@gmail.com>
2020-05-16 13:09:23 +0200

    Initial commit

---
+++ ecommerce-back/controllers/category.js
@@ -0,0 +1,67 @@
+const Category = require('../models/category')
+
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+if (err || !category) {
+    return res.status(400).json({
+        error: 'Category does not exist'
+    })
+}
+    req.category = category
+    next()
+    })
+}
+exports.create = (req, res) => {
+    const category = new Category(req.body)
+    category.save((err, data) => {
+        if(err) {
+            console.log(err)
+            return res.status(400).json({
+                error: 'Could not save category. Try again.'
+            })
+        }
+        res.json({ data })
+        console.log(req.body)
+    })
+}
+
+exports.read = (req, res) => {
+    return res.json(req.category)
+}
+
+exports.update = (req, res) => {
+ const category = req.category
+ category.name = req.body.name
+ category.save((err, data) => {
+if (err) {
+    return res.status(400).json({
+        error: errorHandler(err)
+    })
+}
+res.json(data)
+  })   
+}
+
+exports.remove = (req, res) => {
+    const category = req.category
+    category.remove((err, data) => {
+   if (err) {
+       return res.status(400).json({
+           error: errorHandler(err)
+       })
+   }
+   res.json({
+       message: 'Category deleted'
+   })
+     })   
+   }
+
+exports.list = (req, res) => {
+    Category.findById().exec((err, data) => {
+        return res.status(400).json({
+            error: 'cant create category'
+        })
+    })
+    res.json(data)
+}
---
+++ ecommerce-front/src/core/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+ const Home = () => {
+    return (
+        <Layout title="Home Page" description="Node react ecommerce app">
+          
+        </Layout>
+    )
+}
+
+export default Home
